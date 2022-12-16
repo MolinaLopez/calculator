73207b03e92cb10e89b455f3f40d1eb8143b8671
Luciano <lucianomlopez9@gmail.com>
2022-12-16 23:00:57 +0100

    Update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const add = function() {
   return sum;
 }
 
-const minus = function() {
+const subtract = function() {
   let sum = arguments[0];
   for (i = 1; i < arguments.length; i++) {
     sum -= arguments[i];
@@ -29,3 +29,32 @@ const divide = function() {
   }
   return sum;
 }
+
+const operate = function(n1, op, n2) {
+  let numberOne = n1;
+  let numberTwo = n2;
+  let operator = op;
+
+  if (operator === 'opAdd') {
+    return add(numberOne, numberTwo);
+  } else if (operator === 'opSubtract') {
+    return subtract(numberOne, numberTwo);
+  } else if (operator === 'opMultiply') {
+    return multiply(numberOne, numberTwo);
+  } else if (operator === 'opDivide' && numberTwo !== 0) {
+    return divide(numberOne, numberTwo);
+  } else {
+    return 'Error'
+  }
+}
+
+const display = document.getElementById('display');
+let displayValue = '0';
+display.textContent = displayValue;
+
+const numberButtons = document.querySelectorAll('.number-button');
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log(button.textContent);
+  })
+})
